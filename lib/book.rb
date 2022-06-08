147f7b69ea58ab348bd265d45adf6271b306578b
Micaela Frank <micaelalfrank@gmail.com>
2022-06-08 10:21:57 -0400

    commit tree d985d303f41c3d8ab30dcc758d1c899ee07a1ae1

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
 
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book = Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "Mystery"
+
+book = Book.new("The World According to Garp")
+book.title
+book.turn_page
